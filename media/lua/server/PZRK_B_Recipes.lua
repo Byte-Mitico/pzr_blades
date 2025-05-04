07054e7fe3f4dc3d3a284d200089ff222449b91c
Lucas Carvalho Flores <lcf@ic.ufal.br>
2025-05-03 21:41:07 -0300

    Limpa código.

--- media/lua/server/PZRK_B_Recipes.lua
+++ media/lua/server/PZRK_B_Recipes.lua
@@ -7,10 +7,8 @@ function Recipe.OnCreate.SharpenBlade(items, result, player, selected_item)
 
     local maintenance_lvl = player:getPerkLevel(Perks.Maintenance);
     local condition_max = result:getConditionMax();
-    local condition_recovery = condition_max;
+    local condition_recovery = 0;
     local repair_count = 0;
-    local item_full_type = "";
-    local item_type = "";
 
     -- How much can be repaired by shapening at current maintenance skill.
     if maintenance_lvl < 3 then
@@ -20,14 +18,12 @@ function Recipe.OnCreate.SharpenBlade(items, result, player, selected_item)
     -- Gather info about the current blade being repaired.
     local blade_init_condit = 1;
     for i = 0, items:size()-1 do
-        item = items:get(i);
-        item_type = item:getType();
-        item_full_type = item:getFullType();
+        local item = items:get(i);
+        local item_type = item:getType();
         if item_type ~= "Stone" then
             local categories = item:getCategories();
             for j = 0, categories:size()-1 do
                 local category = categories:get(j);
-                print(category);
                 if category == "SmallBlade" or category == "LongBlade" then
                     blade_init_condit = item:getCondition();
                     repair_count = item:getHaveBeenRepaired();
@@ -37,30 +33,25 @@ function Recipe.OnCreate.SharpenBlade(items, result, player, selected_item)
         end
     end
 
-    -- Calculate the final condition after shapening and clamps it.
-    local blade_final_condit = blade_init_condit + condition_recovery;
-    if blade_final_condit > condition_max then
-        blade_final_condit = condition_max;
+    -- Blades slowly degrades after multiple repairs.
+    local condition_limit = condition_max * (1 - 0.05 * (repair_count-1));
+
+    -- Claps at condition maximun.
+    if condition_limit > condition_max then
+        condition_limit = condition_max;
     end
 
-    -- Blades slowly degrades after multiple repairs.
-    local condition_limit = condition_max * (1 - 0.05 * repair_count);
+    -- Clamps minumum at 2. You can always re-sharpen an old blade.
     if condition_limit < 2 then
         condition_limit = 2;
     end
 
-    -- Clamps the final condition to the limit of multiple repairs.
+
+    -- Calculate the final condition after shapening and clamps it.
+    local blade_final_condit = blade_init_condit + condition_recovery;
     if blade_final_condit > condition_limit then
         blade_final_condit = condition_limit;
     end
-
-    print("Blade Init Condit  " .. blade_init_condit);
-    print("Maintenace Lvl     " .. maintenance_lvl);
-    print("Condit Recov       " .. condition_recovery);
-    print("Condit Max         " .. condition_max);
-    print("Final Condit       " .. blade_final_condit);
-    print("Repairs            " .. repair_count);
-    print(result:getClass());
 
     -- Increments the repair count and sets the new condition.
     result:setHaveBeenRepaired(repair_count + 1);
